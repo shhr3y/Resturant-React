15a89f5886741b7285d9dd72fe121225d92aa64d
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-05-04 06:53:36 +0530

    onClick on Cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import Members from './components/ShreyComponent'
+import { DISHES } from './shared/dishes';
+import { MEMBERS} from './shared/shreys';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary" >
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component {
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      dishes : DISHES,
+      members : MEMBERS,
+    }
+
+  }
+
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary" >
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        {/* <Members members = {this.state.members}/> */}
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
