854c9c67a045f4dc31d71d8cbc5f37fe6447c68e
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-05-04 16:54:06 +0530

    Module 1 DONE!!

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -12,28 +12,9 @@ class Menu extends Component{
           }
      }
 
-
      onDishSelected(dish){
           this.setState({selectedDish:dish});
      }
-
-
-     // renderDish(dish){
-     //      if(dish!=null){
-     //           return(<Card>
-     //                     <CardImg width="100%"  src={dish.image} alt={dish.name}/>
-     //                     <CardBody>
-     //                          <CardTitle>{dish.name}</CardTitle>
-     //                          <CardText>{dish.description}</CardText>
-     //                     </CardBody>
-     //                 </Card>
-     //                );
-     //      }
-     //      else{
-     //           return(<div></div>)
-     //      }
-     // }
-
 
     render(){
 
