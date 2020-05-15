5865bfae795ce9d84dac4520b3027fdbd2f22929
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-05-15 19:04:50 +0530

    build

---
+++ build/precache-manifest.77e0232436a666c68dbaf8e75e868ead.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c086517fd2328b3d3a32379ba30c5e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43be0e0f7474e79c00a5",
+    "url": "/static/css/2.7d9277a0.chunk.css"
+  },
+  {
+    "revision": "414c8ca5a743f8e25568",
+    "url": "/static/css/main.23f39f9e.chunk.css"
+  },
+  {
+    "revision": "43be0e0f7474e79c00a5",
+    "url": "/static/js/2.bb5f1520.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb5f1520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414c8ca5a743f8e25568",
+    "url": "/static/js/main.e42f75a5.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
